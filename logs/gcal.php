9ea85bd2a68eeb21307c1c4f53038b0a55129eb0
imeuro <mauro.fioravanzi@gmail.com>
2021-09-25 00:44:37 +0200

    gitignore upd

---
+++ logs/gcal.php
@@ -0,0 +1,4 @@
+<li>test all day event<span class='date'>23 Sep</span></li>
+<li>test event 2<span class='date'>12 Oct</span></li>
+<li>[14:00] test event with time<span class='date'>25 Oct</span></li>
+<li>test event 4<span class='date'>31 Oct</span></li>
